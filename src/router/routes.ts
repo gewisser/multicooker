6dd5a9774a255232adc796b0cca3b34a644004b5
Гаврилов Роман <roman.gavrilov@devunit.info>
2022-10-15 20:59:41 +0300

    first commit

---
+++ src/router/routes.ts
@@ -0,0 +1,19 @@
+import { RouteRecordRaw } from 'vue-router';
+import routesPages from '~pages'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: routesPages,
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
