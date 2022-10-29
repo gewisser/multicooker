dddead10f365937d5003096c7c931e464a02f984
Гаврилов Роман <roman.gavrilov@devunit.info>
2022-10-29 13:35:03 +0300

    test create tag

--- octo-ci.mjs
+++ octo-ci.mjs
@@ -10,3 +10,19 @@ const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 console.log(owner, repo);
 console.log(process.env);
+
+
+await octokit.request(`POST /repos/${owner}/${repo}/git/tags`, {
+  owner,
+  repo,
+  tag: 'v0.0.1',
+  message: 'initial version',
+  object: process.env.GITHUB_SHA,
+  type: 'commit',
+  tagger: {
+    name: 'Roman Gavrilow',
+    email: 'roman@gavrilow.ru'
+  }
+})
+
+GITHUB_SHA
