2747601dc2e5a50db69c9a016814c3920130c1c0
Гаврилов Роман <roman.gavrilov@devunit.info>
2022-10-29 12:15:20 +0300

    octo-ci test

---
+++ octo-ci.mjs
@@ -0,0 +1,11 @@
+import { Octokit } from "@octokit/action";
+
+const octokit = new Octokit();
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+// const data = await octokit.request('GET /user', {})
+// const { login } = data.data
+
+
+console.log(owner, repo);
